bf544337e05181ac369548893f4dffef2de2201b
cairns <lazycairx@gmail.com>
2014-09-05 07:30:11 +0200

    Started working with game states and trying to take user input.

--- core.js
+++ core.js
@@ -23,8 +23,11 @@ var Loader = (function() {
 window.addEventListener('load', function() {
 
     var STATE = 0;
+    var MESSAGE = '';
 
     var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+
     canvas.style.backgroundColor = "#000000";
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -55,29 +58,106 @@ window.addEventListener('load', function() {
             case 2:
                 Map.draw();
                 break;
+            case 3:
+                //Map.draw();
+                break;
         }
     };
     // Want a draw loop that considers execution time for more stable
     window.setInterval(loop, 33);
 
-    document.addEventListener('keydown', function(e) {
-        switch(e.keyCode) {
-            case 87:
-                console.log("UP");
-                Map.up();
-                break;
-            case 83:
-                console.log("DOWN");
-                Map.down();
-                break;
-            case 65:
-                console.log("LEFT");
-                Map.left();
-                break;
-            case 68:
-                console.log("RIGHT");
-                Map.right();
-                break;
+    /*document.addEventListener('keydown', function(e) {
+
+        if (STATE == 2) {
+            switch(e.keyCode) {
+                case 87:
+                    console.log("UP");
+                    Map.up();
+                    break;
+                case 83:
+                    console.log("DOWN");
+                    Map.down();
+                    break;
+                case 65:
+                    console.log("LEFT");
+                    Map.left();
+                    break;
+                case 68:
+                    console.log("RIGHT");
+                    Map.right();
+                    break;
+                case 13:
+                    STATE = 3;
+                    console.log("ENTER");
+                    break;
+            }
+        } else if (STATE == 3) {
+            switch(e.keyCode) {
+                case 13:
+                    MESSAGE = '';
+                    STATE = 2;
+                    break;
+
+                default:
+                    var char = String.fromCharCode(e.keyCode);
+                    if (e.)
+                    MESSAGE += ;
+                    console.log(String.fromCharCode(e.keyCode));
+                    context.font = '16px Arial';
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+                    context.fillText(MESSAGE, 200, 200);
+                    break;
+            }
+        }
+    });*/
+
+    document.addEventListener('keypress', function(e) {
+
+        if (STATE == 2) {
+            switch(e.which) {
+                //case 87:
+                case 119:
+                    console.log("UP");
+                    Map.up();
+                    break;
+                //case 83:
+                case 115:
+                    console.log("DOWN");
+                    Map.down();
+                    break;
+                //case 65:
+                case 97:
+                    console.log("LEFT");
+                    Map.left();
+                    break;
+                //case 68:
+                case 100:
+                    console.log("RIGHT");
+                    Map.right();
+                    break;
+                //case 13:
+                case 13:
+                    STATE = 3;
+                    console.log("ENTER");
+                    break;
+            }
+        } else if (STATE == 3) {
+            switch(e.which) {
+                case 13:
+                    MESSAGE = '';
+                    STATE = 2;
+                    break;
+
+                default:
+                    console.log(e);
+                    var char = String.fromCharCode(e.charCode);
+                    MESSAGE += char;
+                    console.log(String.fromCharCode(e.keyCode));
+                    context.font = '16px Arial';
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+                    context.fillText(MESSAGE, 200, 200);
+                    break;
+            }
         }
     });
 });
