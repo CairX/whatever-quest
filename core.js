7b66b9bd8e14d824647e1c6fca041bc7f6bcc7e3
cairns <lazycairx@gmail.com>
2014-09-07 17:06:16 +0200

    Set username when the game starts.

--- core.js
+++ core.js
@@ -21,30 +21,97 @@ var Loader = (function() {
 })();
 
 var Message = (function() {
+    var log = [];
     var message = '';
     var self = {};
 
     self.draw = function(context) {
         context.font = '16px Arial';
-        context.fillStyle = '#0000FF';
-        context.fillText(message, 200, 200);
+        context.fillStyle = '#01579b';
+
+        var latest = log.length < 10 ? log : log.slice(log.length - 10);
+        for (var i = 0; i < latest.length; i ++) {
+            context.fillText(latest[i], 200, (200+(20*i)));
+        }
+
+        context.fillText(message, 200, 800);
     };
 
-    self.listener = function(e) {
-        switch(e.which) {
-            case 13:
+    self.listener = function(event) {
+        switch(event.which) {
+            // Escape
+            case 0:
                 message = '';
                 Game.setState(2);
                 break;
 
+            // Enter
+            case 13:
+                log.push(message);
+                message = '';
+                Game.setState(2);
+                break;
+
+            // Backspace
             case 8:
                 message = message.substring(0, (message.length - 1));
                 break;
 
             default:
-                console.log(e);
-                message += String.fromCharCode(e.charCode);
-                e.stopPropagation();
+                console.log(event);
+                message += String.fromCharCode(event.charCode);
+                event.stopPropagation();
+                break;
+        }
+    };
+
+    return self;
+})();
+
+var Login = (function() {
+    var self = {};
+    var width = 364;
+    var height = 120;
+
+    var username = '';
+
+    self.draw = function (context) {
+        context.fillStyle = '#e1f5fe';
+        context.fillRect((Game.center.x - width / 2), (Game.center.y - height / 2), width, height);
+
+        context.fillStyle = 'white';
+        context.fillRect((Game.center.x - 300 / 2), (Game.center.y - height / 2 + 64), 300, 24);
+
+        context.font = '16px Arial';
+        context.fillStyle = '#01579b';
+        context.textAlign = 'center';
+        context.textBaseline = 'top';
+        context.fillText('Username', Game.center.x, (Game.center.y - height / 2 + 32));
+
+        context.fillText(username + '|', Game.center.x, ((Game.center.y - height / 2 + 68)));
+    };
+
+    self.listener = function(event) {
+        switch(event.which) {
+            // Escape
+            case 0:
+                break;
+
+            // Enter
+            case 13:
+                Cookie.set('username', username);
+                Game.setState(0);
+                break;
+
+            // Backspace
+            case 8:
+                username = username.substring(0, (username.length - 1));
+                break;
+
+            default:
+                console.log(event);
+                username += String.fromCharCode(event.charCode);
+                event.stopPropagation();
                 break;
         }
     };
@@ -53,23 +120,34 @@ var Message = (function() {
 })();
 
 var Game = (function() {
-    var state = 0;
+    var state;
     var canvas;
     var context;
 
+
     var self = {};
+    self.size = { width: 0, height: 0 };
+    self.center = { x: 0, y: 0 };
 
     self.init = function() {
         canvas = document.getElementById('canvas');
         context = canvas.getContext('2d');
 
-        canvas.style.backgroundColor = "#000000";
+        canvas.style.backgroundColor = "#b3e5fc";
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
+
+        self.size.width = canvas.width;
+        self.size.height = canvas.height;
+
+        self.center.x = Math.floor(canvas.width / 2);
+        self.center.y = Math.floor(canvas.height / 2);
 
         // Display message asking for name if not set.
         Cookie.set('username', 'cairns');
         Resources.init();
+
+        self.setState(4);
 
         // Want a draw loop that considers execution time for smoother effect.
         window.setInterval(self.run, 33);
@@ -91,22 +169,40 @@ var Game = (function() {
                 window.onresize = function() {
                     canvas.width = window.innerWidth;
                     canvas.height = window.innerHeight;
+
+                    self.size.width = canvas.width;
+                    self.size.height = canvas.height;
+
+                    self.center.x = Math.floor(canvas.width / 2);
+                    self.center.y = Math.floor(canvas.height / 2);
+
                     Map.resize();
                 };
                 self.setState(2);
                 break;
             case 2:
-                Map.draw();
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                Map.draw(context);
+                Message.draw(context);
                 break;
             case 3:
-                Map.draw();
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                Map.draw(context);
                 Message.draw(context);
+                break;
+
+            case 4:
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                Login.draw(context);
                 break;
         }
     };
 
     self.setState = function(s) {
         switch(s) {
+            case 0:
+                document.removeEventListener('keypress', Login.listener, true);
+                break;
             case 2:
                 console.log('TWO');
                 document.removeEventListener('keypress', Message.listener, true);
@@ -116,6 +212,9 @@ var Game = (function() {
                 console.log('THREE');
                 document.removeEventListener('keydown', Map.listener, true);
                 document.addEventListener('keypress', Message.listener, true);
+                break;
+            case 4:
+                document.addEventListener('keypress', Login.listener, true);
                 break;
         }
         state = s;
