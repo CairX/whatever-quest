601fa061c31218e308eab2c78c4895c3271c7fc5
cairns <lazycairx@gmail.com>
2014-09-11 19:51:15 +0200

    Updated the server keep connections alive. Implemented a kind of broadcast feature over connections (pretty sure this can be done another way).

--- server.py
+++ server.py
@@ -1,15 +1,67 @@
 import asyncio
+import json
 import websockets
 
-@asyncio.coroutine
-def hello(websocket, path):
-    name = yield from websocket.recv()
-    print("< {}".format(name))
-    gretting = "Hello {}!".format(name)
-    yield from websocket.send(gretting)
-    print(("> {}").format(gretting))
+users = list()
 
-start_server = websockets.serve(hello, '0.0.0.0', 8765)
+
+class User(object):
+    def __init__(self, name, websocket):
+        self.name = name
+        self.websocket = websocket
+
+
+@asyncio.coroutine
+def broadcast(message):
+    for user in users:
+        if user.websocket.open:
+            yield user.websocket.send(message)
+        else:
+            try:
+                users.remove(user)
+            except ValueError:
+                pass
+
+
+@asyncio.coroutine
+def login(username, websocket):
+    exists = False
+
+    for user in users:
+        if username == user.name:
+            exists = True
+            break
+
+    if exists:
+        yield from websocket.send('{ "action": "login", "status": "exists" }')
+    else:
+        user = User(username, websocket)
+        users.append(user)
+        yield from websocket.send('{ "action": "login", "status": "success" }')
+
+
+@asyncio.coroutine
+def handler(websocket, path):
+    #connections.append(websocket)
+
+    while True:
+        message = yield from websocket.recv()
+        if message is None:
+            print("Remove connection")
+            break
+
+        message = json.loads(message)
+        print(message)
+        action = message.get('action')
+        username = message.get('username')
+
+        if action == 'login':
+            yield from login(username, websocket)
+        elif action == 'message':
+            broadcast(message.get('message'))
+
+
+start_server = websockets.serve(handler, '0.0.0.0', 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
