8bad30474c73d75fc8f3f7176edc5b2554be6261
cairns <lazycairx@gmail.com>
2014-09-13 09:49:16 +0200

    Moved connection to it's own file.

---
+++ connection.js
@@ -0,0 +1,30 @@
+var Connection = (function() {
+    var socket;
+
+    var self = {};
+
+    self.init = function() {
+        socket = new WebSocket("ws://localhost:8765");
+
+        socket.onopen = function() {
+            console.log('Connection opened.')
+        };
+        socket.onmessage = function (evt) {
+            var received_msg = evt.data;
+            console.log("MESSAGE: " + received_msg);
+            var data = JSON.parse(received_msg);
+            console.log(data);
+            console.log(data.action);
+            //Game.received(data);
+        };
+        socket.onclose = function() {
+            console.log("Connection closed.");
+        };
+    };
+
+    self.send = function(data) {
+        socket.send(JSON.stringify(data));
+    };
+
+    return self;
+})();
